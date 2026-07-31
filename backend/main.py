5e2822f27991c45e98ad27e77b44db65e6afebf9
Jorge <jorgesolermartinez@gmail.com>
2026-07-31 18:35:22 +0200

    M1.03 Create application layout

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="RentalManager",
     version="1.0.0"
 )
 
+app.mount(
+    "/static",
+    StaticFiles(directory="backend/static"),
+    name="static"
+)
+
+templates = Jinja2Templates(directory="backend/templates")
+
 
 @app.get("/")
-def home():
-    return {
-        "application": "RentalManager",
-        "status": "running",
-        "version": "1.0.0"
-    }
+def dashboard(request: Request):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="pages/dashboard.html",
+        context={
+            "request": request,
+            "version": app.version,
+            "current_page": "dashboard",
+        },
+    )
